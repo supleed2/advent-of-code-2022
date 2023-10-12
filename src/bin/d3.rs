9f6c7656d57720673e5e60ec451421e32214b6d5
Aadi Desai <21363892+supleed2@users.noreply.github.com>
2023-10-12 11:28:58 +0100

    Day 3, Part 1 & 2

---
+++ src/bin/d3.rs
@@ -0,0 +1,40 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../input/d3.txt");
+
+fn main() -> anyhow::Result<()> {
+    let p1 = INPUT
+        .split('\n')
+        .map(|s| s.split_at(s.len() / 2))
+        .map(|(l, r)| {
+            l.chars().find(|ch| {
+                r.contains(*ch)
+            }).expect("No common characters in rucksack")
+        })
+        .map(item_priority)
+        .sum::<usize>();
+    println!("{p1}");
+    let p2 = INPUT
+        .split('\n')
+        .batching(|it| {
+            it.next_tuple::<(&str,&str,&str)>()
+        })
+        .map(|(a, b, c)| {
+            a.chars()
+                .filter(|&ch| b.contains(ch))
+                .find(|&ch| c.contains(ch))
+                .expect("No common characters in 3 rucksacks")
+        })
+        .map(item_priority)
+        .sum::<usize>();
+    println!("{p2:?}");
+    anyhow::Ok(())
+}
+
+fn item_priority(item: char) -> usize {
+    match item {
+        'a'..='z' => item as usize - 96,
+        'A'..='Z' => item as usize - 38,
+        _ => unreachable!(),
+    }
+}
